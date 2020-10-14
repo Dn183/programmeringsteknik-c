c12edc3003fff5f261b8828e723b9385edf1e50e
LAPTOP-A4P43CG4\jacka <jacob.lindqvist@studerande.plushogskolan.se>
2020-10-14 08:38:28 +0200

    no message

--- friday/tgif/Program.cs
+++ friday/tgif/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Dynamic;
+using System.Globalization;
+using System.Net.Http.Headers;
 
 namespace tgif
 {
@@ -8,6 +11,31 @@ namespace tgif
         {
             // Skriv en applikation som läser in ett datum via användarinmatning,
             // som sedan räknar ut hur många dagar det är till nästa fredag.
+            //från det inmatade datumet
+
+            DateTime date;
+            while (!GetDateFromUser(out date));
+
+            DayOfWeek givenDay = date.DayOfWeek;
+            DayOfWeek friday = DayOfWeek.Friday;
+
+            int difference = friday - givenDay;
+
+            if (difference < 0)
+                difference += 7;
+
+            Console.WriteLine($"There are {difference} days until next friday");
+        }
+
+        static bool GetDateFromUser(out DateTime date)
+        {
+            Console.WriteLine("Enter a date");
+
+            string input = Console.ReadLine();
+
+            CultureInfo culture = new CultureInfo("sv-SE");
+
+            return DateTime.TryParse(input, culture, DateTimeStyles.AssumeLocal, out date);
         }
     }
 }
--- vowels/vowelapp/Program.cs
+++ vowels/vowelapp/Program.cs
@@ -1,13 +1,47 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace vowelapp
 {
     class Program
     {
+        char[] _vowels;
+        
+        public char[] Vowels { get; set;}
+
         static void Main(string[] args)
         {
             // Skriv en konsolapplikation som tar bort vokaler (konstigt va?) från en inmatad sträng.
             // Applikationen skall både presentera den resulterande strängen och antalet vokaler som togs bort.
+
+            char[] vowels = new char[] { 'a', 'e', 'o', 'i', 'u', 'y', 'å', 'ä', 'ö' };
+
+            Console.WriteLine("Skriv en stäng: ");
+            var input = Console.ReadLine().ToLower();
+            var output = new List<char>();
+            int removedCount = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                var currentCharacter = input[i];
+                var normalizedCharacter = char.ToLower(currentCharacter);
+
+                if (vowels.Contains(currentCharacter) == false)
+                {
+                    output.Add(currentCharacter);
+                }
+                else
+                {
+                    removedCount++;
+                }
+            }
+
+
+            var resultingString = new string(output.ToArray());
+
+            Console.WriteLine($"Din sträng utan vokaler '{resultingString}', vi tog bort {removedCount} st.");
+
         }
     }
 }
